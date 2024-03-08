d115ec1d7a49fbb803245d216694de08e83d4587
estefargu <earciniegas100324@umanizales.edu.co>
2024-03-08 17:35:34 -0500

    conexion con la base de datos

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
+const connection = require('./DataBase/connection');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 1337;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+connection.sync({force: false}) // metodo que sincroniza la base de datos
+    .then(() => { 
+        console.log('Base de datos sincronizada');
+        app.listen(port, function() { // NOS VA A ESCUCHAR POR EL PUERTO QUE LE DEFINIMOS
+            console.log('The application is running on port' + port);
+        })
+    })  
+    .catch((error) =>{
+        console.error('Error syncing DataBase' + error);
+    });
