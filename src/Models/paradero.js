6beb6cdf5445179c26bea3fa125feabf41389dbf
estefargu <earciniegas100324@umanizales.edu.co>
2024-03-11 11:22:16 -0500

    borre hora fin

--- src/Models/paradero.js
+++ src/Models/paradero.js
@@ -16,10 +16,6 @@ paradero.init({
     horaInicio:{
         type: DataTypes.STRING,
         allowNull: false
-    },
-    horaFin:{
-        type: DataTypes.STRING,
-        allowNull: false
     }
 },{
     sequelize: connection,
