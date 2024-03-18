06d3c171f6cdc692f03c0c9e554d777b98515ec1
estefargu <earciniegas100324@umanizales.edu.co>
2024-03-18 10:20:37 -0500

    server

--- src/server.js
+++ src/server.js
@@ -1,18 +1,31 @@
+require('./DataBase/sync.js');
+
 const connection = require('./DataBase/connection');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 1337;
 
+//routers
+const conductorrouter = require('./Routers/conductorrouter.js');
+const empresatransporterouter = require('./Routers/empresatransporterouter.js');
+const paraderorouter = require('./Routers/paraderorouter.js');
+const rutaparaderorouter = require('./Routers/rutaparaderorouter.js');
+const rutarouter = require('./Routers/rutarouter.js');
+const usuariorouter = require('./Routers/usuariorouter.js');
+const vehiculorouter = require('./Routers/vehiculorouter.js');
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-connection.sync({force: false}) // metodo que sincroniza la base de datos
-    .then(() => { 
-        console.log('Base de datos sincronizada');
-        app.listen(port, function() { // NOS VA A ESCUCHAR POR EL PUERTO QUE LE DEFINIMOS
-            console.log('The application is running on port' + port);
-        })
-    })  
-    .catch((error) =>{
-        console.error('Error syncing DataBase' + error);
-    });
+app.listen(port, function() { // NOS VA A ESCUCHAR POR EL PUERTO QUE LE DEFINIMOS
+    console.log('The application is running on port' + port);
+})
+
+//api
+app.use('/api', conductorrouter);
+app.use('/api', empresatransporterouter);
+app.use('/api', paraderorouter);
+app.use('/api', rutaparaderorouter);
+app.use('/api', rutarouter);
+app.use('/api', usuariorouter);
+app.use('/api', vehiculorouter);    
